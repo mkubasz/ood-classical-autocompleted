f20e87cc0561e1462bcc2535c7e74bd3c79213da
GitHub Action <action@github.com>
2026-04-08 16:17:10 +0000

    Template cleanup

--- src/main/kotlin/com/github/mkubasz/oodclassicalautocompleted/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/mkubasz/oodclassicalautocompleted/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.mkubasz.oodclassicalautocompleted.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
